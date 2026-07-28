455efa10b65a9ff0120652af3519edb944e38023
Peter H. Boling <peter.boling@gmail.com>
2026-07-28 13:27:36 -0600

    🔖 Bump gem version

--- lib/stone_checksums/version.rb
+++ lib/stone_checksums/version.rb
@@ -4,7 +4,7 @@ module StoneChecksums
   # Version namespace for this gem.
   module Version
     # Current gem version.
-    VERSION = "1.0.7"
+    VERSION = "1.0.8"
   end
   # Current gem version exposed at the traditional constant location.
   VERSION = Version::VERSION # Traditional Constant Location
